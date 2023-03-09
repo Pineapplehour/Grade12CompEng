1f0132f376b642027ff9ac4eff4a1e2c4b2bea99
Pineapplehour <91144645+Pineapplehour@users.noreply.github.com>
2023-03-09 14:18:20 -0500

    Add files via upload

---
+++ Arduino Files/JunkCode.ino
@@ -0,0 +1,21 @@
+int Q = 2;
+int QBAR = 3;
+int PUSH_BUTTON = 5;
+int DIP_SWITCH_2 = 6;
+int DIP_SWITCH_1 = 7;
+int BAUD_RATE = 9600;
+bool q;
+bool qBar;
+void setup() {
+  Serial.begin(BAUD_RATE);
+  pinMode(Q, OUTPUT);
+  pinMode(QBAR, OUTPUT);
+  pinMode(PUSH_BUTTON, INPUT);
+  pinMode(DIP_SWITCH_2, INPUT);
+  pinMode(DIP_SWITCH_1, INPUT);
+}
+
+void loop() {
+  digitalWrite(Q, HIGH);
+  digitalWrite(QBAR, HIGH);
+}

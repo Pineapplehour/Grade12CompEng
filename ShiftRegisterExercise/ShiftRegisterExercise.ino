1d4e1ae440b03fc4f70b8fa3320ae5c9efc6cd32
Pineapplehour <91144645+Pineapplehour@users.noreply.github.com>
2023-03-20 23:17:18 -0500

    Create ShiftRegisterExercise.ino

---
+++ ShiftRegisterExercise/ShiftRegisterExercise.ino
@@ -0,0 +1,38 @@
+int DS_PIN = 0;
+int LATCH_PIN = 0;
+int CLOCK_PIN = 0;
+int BAUD_RATE = 9600;
+int DELAY_LENGTH = 100; 
+int patternArray[9][8] = {
+  {0,0,0,0,0,0,0,0},
+  {1,0,0,0,0,0,0,0},
+  {0,1,0,0,0,0,0,0},
+  {0,0,1,0,0,0,0,0},
+  {0,0,0,1,0,0,0,0},
+  {0,0,0,0,1,0,0,0},
+  {0,0,0,0,0,1,0,0},
+  {0,0,0,0,0,0,1,0},
+  {0,0,0,0,0,0,0,1}
+};
+void setup() {
+  Serial.begin(BAUD_RATE);
+  pinMode(DS_PIN, OUTPUT);
+  pinMode(LATCH_PIN, OUTPUT);
+  pinMode(CLOCK_PIN, OUTPUT);
+}
+
+void loop() {
+  for(int r = 0; r < 9; r++){
+    for(int c = 0; c < 8; c++){
+      digitalWrite(DS_PIN, patternArray[r][c]);
+      pulsePin(CLOCK_PIN, DELAY_LENGTH);
+    }
+    pulsePin(LATCH_PIN, DELAY_LENGTH);
+  }
+}
+
+void pulsePin(int pin, int delayLength){
+  digitalWrite(pin, HIGH);
+  delay(delayLength);
+  digitalWrite(pin, LOW);
+}

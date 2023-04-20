1522c74349a32ee1de9545b7c5dfd9454d309307
Pineapplehour <thanesapineapple@gmail.com>
2023-04-19 23:45:01 -0400

    freshing it up

--- MesurementTool/MesurementTool.ino/MesurementTool.ino.ino
+++ MesurementTool/MesurementTool.ino/MesurementTool.ino.ino
@@ -23,15 +23,15 @@ char keys[ROWS][COLS] = {
 byte rowPins[ROWS] = {14, 15, 16, 17}; // connect to the row pinouts of the keypad
 byte colPins[COLS] = {18, 19, 20, 21}; // connect to the column pinouts of the keypad
 Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS); 
-int lastMode = 0; 
-int lastRef= 0;
-String tempRef = "";
-long tempRefInMs = 0;
+int mode = 0; 
+int referenceIndex= 0;
+String temporaryReference = "";
+long temporaryReferenceInMs = 0;
 long lastDistanceInMs = 0;
 boolean refSet = true;
 boolean useCM = true; 
 boolean matchFound = false; 
-long refArray[5] = {200,400,800,1600,3200};
+long referenceArray[5] = {200,400,800,1600,3200};
 
 void setup() {
  Serial.begin(9600);
@@ -48,20 +48,22 @@ void loop() {
   updateMode();
   updateUnits();
   /*
-  mode name | last mode value
+  mode name | mode value
   measure   | 0
   hold      | 1
   reference | 2
   */
-  if(lastMode == 0){
+  if(mode == 0){ 
     measure();
   }
-  else if(lastMode == 1){
+  else if(mode == 1){
     hold();
   }
-  else if(lastMode == 2){
+  else if(mode == 2){
     referenceMode();
   }
+  Serial.println();
+  delay(250);
 }
 
 long distanceInMs(){
@@ -70,6 +72,13 @@ long distanceInMs(){
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
   lastDistanceInMs = (pulseIn(echoPin, HIGH)*3 + lastDistanceInMs*2)/5;
+  if(lastDistanceInMs/58 > 400){
+    return(-1);
+  }
+  else if (lastDistanceInMs/58 < 2) {
+    return(-2);
+  }
+  else{}
   return (lastDistanceInMs);
 }
 
@@ -86,16 +95,16 @@ int readKeys(){
 
 void updateMode(){
   //when up key is pressed, the mode cycles. 
-  if(readKeys() == 1 && lastMode != 2){
-    lastMode++ ;
+  if(readKeys() == 1 && mode != 2){
+    mode++ ;
     while(readKeys() == 1){}
   }
-  else if(readKeys() == 1 && lastMode == 2){
-    lastMode = 0; 
+  else if(readKeys() == 1 && mode == 2){
+    mode = 0; 
     while(readKeys() == 1){}
   }
-  Serial.print(" lastMode: ");
-  Serial.print(lastMode);
+  Serial.print(" Mode: ");
+  Serial.print(mode);
 }
 
 void updateUnits(){
@@ -104,7 +113,7 @@ void updateUnits(){
     while(readKeys() == 4){}
   }
   Serial.print(" Using CM: ");
-  Serial.println(useCM);
+  Serial.print(useCM);
 }
 
 void measure(){
@@ -113,29 +122,38 @@ void measure(){
   lcd.print("MEASURE");
   lcd.setCursor(0,1);
   long tempMeasurement = distanceInMs();
-  if(useCM == true){
-    lcd.print(tempMeasurement/58);
-    lcd.print("cm");
+  if(tempMeasurement == -1){
+    lcd.print("Out of Range");
   }
-  else if(useCM == false){
-    lcd.print(tempMeasurement/148);
-    lcd.print("\"");    
+  else if(tempMeasurement == -2){
+    lcd.print("Too close");
   }
-  matchFound = false; 
-  for(int i = 0; i < 4 && !matchFound; i++){
-    if(refArray[i] + 50 > tempMeasurement && refArray[i] - 50 < tempMeasurement){
-      lcd.print(" =R");
-      lcd.print(i + 1);
-      lcd.print("/5");
-      matchFound = true; 
+  else{
+    if(useCM == true){
+      lcd.print(tempMeasurement/58);
+      lcd.print("cm");
+    }
+    else if(useCM == false){
+      lcd.print(tempMeasurement/148);
+      lcd.print("\"");    
+    }
+    matchFound = false; 
+    for(int i = 0; i < 4 && !matchFound; i++){
+      if(referenceArray[i] + 50 > tempMeasurement && referenceArray[i] - 50 < tempMeasurement){
+        lcd.print(" =R");
+        lcd.print(i + 1);
+        lcd.print("/5");
+        matchFound = true; 
+      }
     }
   }
-  Serial.print("Measured: ");
-  Serial.println(tempMeasurement);
-  delay(250);
+  
+  Serial.print(" Measured: ");
+  Serial.print(tempMeasurement);
 }
 
 void hold(){
+  lcd.clear();
   long held = distanceInMs();
   while(readKeys() != 1){
     updateUnits();
@@ -150,31 +168,29 @@ void hold(){
       lcd.print(held/148);
       lcd.print("\"");
     }
-    delay(250);
-    lcd.clear();
   }
   
 }
 
 void updateRef(){
-  if(readKeys() == 0 && lastRef != 4){
-    lastRef++;
+  if(readKeys() == 0 && referenceIndex != 4){
+    referenceIndex++;
     while(readKeys() == 0){}
   }
-  else if(readKeys() == 0 && lastRef == 4 ){
-    lastRef = 0; 
+  else if(readKeys() == 0 && referenceIndex == 4 ){
+    referenceIndex = 0; 
     while(readKeys() == 0){}
   }
-  if(readKeys() == 3 && lastRef != 0){
-    lastRef--;
+  if(readKeys() == 3 && referenceIndex != 0){
+    referenceIndex--;
     while(readKeys() == 3){}
   }
-  else if(readKeys() == 3 && lastRef == 0){
-    lastRef = 4;
+  else if(readKeys() == 3 && referenceIndex == 0){
+    referenceIndex = 4;
     while(readKeys() == 3){}
   }
-  Serial.print(" last Ref: ");
-  Serial.print(lastRef);
+  Serial.print(" Reference Index: ");
+  Serial.print(referenceIndex);
 }
 
 void referenceMode(){
@@ -182,41 +198,30 @@ void referenceMode(){
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("REFERENCE: ");
-  lcd.print(lastRef + 1);
+  lcd.print(referenceIndex + 1);
 
   char key = keypad.getKey();
   if (key){
-    tempRef = tempRef + key;
-    if(useCM = true){
-      tempRefInMs = tempRef.toInt()*58;
+    temporaryReference = temporaryReference + key;
+    if(useCM == true){
+      temporaryReferenceInMs = temporaryReference.toInt()*58;
     }
-    else if(useCM = false){
-      tempRefInMs = tempRef.toInt()*148;
+    else if(useCM == false){
+      temporaryReferenceInMs = temporaryReference.toInt()*148;
     }
     refSet = false;
   }
   
-  if(readKeys() == 2){
-    if(useCM == true){
-      refArray[lastRef] = tempRef.toInt()*58; 
-    }
-    if(useCM == false){
-      refArray[lastRef] = tempRef.toInt()*148; 
-    }
-    while(readKeys() == 2){}
-    refSet = true;
-    tempRef = "";
-    tempRefInMs = 0;
-  }
+  
 
   if(refSet == true){
     lcd.setCursor(0, 1);
     if(useCM == true){
-      lcd.print(refArray[lastRef]/58);
+      lcd.print(referenceArray[referenceIndex]/58);
       lcd.print("cm");
     }
     else if(useCM == false){
-      lcd.print(refArray[lastRef]/148);
+      lcd.print(referenceArray[referenceIndex]/148);
       lcd.print("\"");
     }
   }
@@ -224,13 +229,36 @@ void referenceMode(){
   if(refSet == false){
     lcd.setCursor(0, 1);
     if(useCM == true){
-      lcd.print(tempRefInMs/58);
+      lcd.print(temporaryReferenceInMs/58);
       lcd.print("cm");
     }
     else if(useCM == false){
-      lcd.print(tempRefInMs/148);
+      lcd.print(temporaryReferenceInMs/148);
       lcd.print("\"");
     }
+  }
+
+  if(readKeys() == 2){
+    if(temporaryReference.toInt() > 400 && useCM == true){
+      lcd.setCursor(0, 1);
+      lcd.print("Out of range");
+      delay(500);
+    }
+    else if (temporaryReference.toInt() > 157 && useCM == false) {
+      lcd.setCursor(0, 1);
+      lcd.print("Out of range");
+      delay(500);
+    }
+    else if(useCM == true){
+      referenceArray[referenceIndex] = temporaryReference.toInt()*58; 
+    }
+    else if(useCM == false){
+      referenceArray[referenceIndex] = temporaryReference.toInt()*148; 
+    }
+    while(readKeys() == 2){}
+    refSet = true;
+    temporaryReference = "";
+    temporaryReferenceInMs = 0;
   }
 }
 
